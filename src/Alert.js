3bf67a4241816259f395eff7f1193aea01052bc2
Ashley Christensen <ashleyccode@yahoo.com>
2022-04-05 12:15:03 -0700

    local storage and additional functionality

--- src/Alert.js
+++ src/Alert.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 
-const Alert = ({ type, message, removeAlert }) => {
+const Alert = ({ list, type, message, removeAlert }) => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       removeAlert();
     }, 3000);
-  }, []);
+    return () => clearTimeout(timeout);
+  }, [list]);
   return <p className={`alert alert-${type}`}>{message}</p>;
 };
 
--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import List from './List';
 import Alert from './Alert';
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return JSON.parse(localStorage.getItem('list'));
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [alert, setAlert] = useState({
     show: false,
     message: '',
@@ -17,9 +26,9 @@ function App() {
     if (!name) {
       //display alert
 
-      displayAlert(true, 'fail', 'Try adding a value');
+      displayAlert(true, 'fail', 'Please add item to list');
     } else {
-      // show alert
+      displayAlert(true, 'success', 'item added to the list');
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName('');
@@ -31,32 +40,42 @@ function App() {
   };
 
   const removeItem = (id) => {
+    displayAlert(true, 'fail', `item removed`);
     setList(list.filter((item) => item.id !== id));
   };
   const clearItems = () => {
+    displayAlert(true, 'fail', 'Grocery list emptied');
     setList([]);
   };
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
   return (
     <section className='section-center'>
       <form>
-        {alert.show && <Alert {...alert} removeAlert={displayAlert} />}
-        <h2 className=''>E-Grocier</h2>
-        <input
-          className='input'
-          type='text'
-          placeholder='e.g. avocado'
-          id='item'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        ></input>
-        <button className='btn' type='submit' onClick={handleSubmit}>
-          Submit!
-        </button>
-        {list.length > 0 && (
-          <button className='clear-btn' onClick={clearItems}>
-            Clear Items
-          </button>
+        {alert.show && (
+          <Alert list={list} {...alert} removeAlert={displayAlert} />
         )}
+        <h2 className='container-center'>E-Grocier</h2>
+        <div className='div-center'>
+          <input
+            className='input'
+            type='text'
+            placeholder='e.g. avocado'
+            id='item'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+          <button className='btn' type='submit' onClick={handleSubmit}>
+            Submit!
+          </button>
+          {list.length > 0 && (
+            <button className='clear-btn' onClick={clearItems}>
+              Clear Items
+            </button>
+          )}
+        </div>
       </form>
       <article className='list-item'>
         <List items={list} removeItem={removeItem} />
